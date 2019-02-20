d63f0a25d0ba1e5577635ce49452df01952e6038
AlexsandrDowgaluk <Dowgaluk88@gmail.com>
2019-02-20 13:20:34 +0200

    Commit 1

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -2,12 +2,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.ITestResult;
-import org.testng.TestNG;
+
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.internal.TestResult;
 
 import java.awt.*;
 import java.io.IOException;
@@ -16,15 +15,16 @@ public class Start {
 
     @DataProvider(name = "test1")
     public Object[][] createData1() {
-        return new Object[][] {
-                { "Dowgaluk", "7" , "0939517619"},
-                { "Dowgaluktest", "7" , "0939517619"},
+        return new Object[][]{
+                {"Dowgaluk", "7", "0939517619"},
+                {"Dowgaluktest", "7", "0939517619"},
 
 
         };
     }
+
     @Test(dataProvider = "test1")
-    public void employee(String n1,String n2 ,String  n3) throws AWTException, IOException {
+    public void employee(String n1, String n2, String n3) {
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://localhost:8080/test5/employee");
@@ -40,29 +40,25 @@ public class Start {
         Assert.assertTrue(addEmployee.comparisonNumber().contains(addEmployee.c));
 
 
-
-
         driver.quit();
 
 
     }
 
     @AfterMethod
-    public void screenBag(ITestResult result) throws IOException, AWTException {
+    public void screenBag(ITestResult result) throws IOException, AWTException, InterruptedException {
 
-if (result.getStatus()== ITestResult.FAILURE)
-{
-    ScreenShot photo = new ScreenShot();
-    photo.screenshot();
-}
-if (result.getStatus()==ITestResult.SUCCESS){
-    System.out.println("Все ок");
-}
+        if (result.getStatus() == ITestResult.FAILURE) {
+            Thread.sleep(1000);
+            ScreenShot photo = new ScreenShot();
+            photo.screenshot();
+        }
+        if (result.getStatus() == ITestResult.SUCCESS) {
+            System.out.println("Все ок");
+        }
 
 
     }
-
-
 
 
 }
